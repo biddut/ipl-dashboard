35e22deea0d585f1a1613b7939a5d4b0ec86be3a
biddut <biddut.bhowmic@gmail.com>
2021-11-13 22:33:14 -0500

    Added Team view API

---
+++ src/main/java/com/javabi/ipldashboard/model/Team.java
@@ -0,0 +1,85 @@
+package com.javabi.ipldashboard.model;
+
+import java.util.List;
+import com.javabi.ipldashboard.model.Match;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    @Transient
+    private List<Match> matches;
+
+    public Team() {
+    }
+
+    public Team(String teamName, long totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
+
+    public Team(long id, String teamName, long totalMatches, long totalWins) {
+        this.id = id;
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+        this.totalWins = totalWins;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public long getTotalMatches() {
+        return totalMatches;
+    }
+
+    public void setTotalMatches(long totalMatches) {
+        this.totalMatches = totalMatches;
+    }
+
+    public long getTotalWins() {
+        return totalWins;
+    }
+
+    public void setTotalWins(long totalWins) {
+        this.totalWins = totalWins;
+    }
+
+    public List<Match> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(List<Match> list) {
+        this.matches = list;
+    }
+
+    @Override
+    public String toString() {
+        return "Team [id=" + id + ", teamName=" + teamName + ", totalMatches=" + totalMatches + ", totalWins="
+                + totalWins + "]";
+    }
+
+}
---
+++ src/main/java/com/javabi/ipldashboard/repository/TeamRepository.java
@@ -0,0 +1,11 @@
+package com.javabi.ipldashboard.repository;
+
+import com.javabi.ipldashboard.model.Team;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamRepository extends CrudRepository<Team, Long> {
+
+    Team findByTeamName(String teamName);
+
+}
